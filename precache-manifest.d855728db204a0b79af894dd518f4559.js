a75d1a5c2bbedf9225ac999e68460086cea9155f
Kacper Krojna <kacper.krojna@gmail.com>
2020-09-03 07:57:01 +0200

    Updates

---
+++ precache-manifest.d855728db204a0b79af894dd518f4559.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c44c35b88e167ffd5efb4ce5eb9a86d",
+    "url": "/swapi/index.html"
+  },
+  {
+    "revision": "b0dfed35e4b0d8704914",
+    "url": "/swapi/static/css/main.18a3be77.chunk.css"
+  },
+  {
+    "revision": "86fc5e833aa08013f4a5",
+    "url": "/swapi/static/js/2.9a6f6e2b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/swapi/static/js/2.9a6f6e2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0dfed35e4b0d8704914",
+    "url": "/swapi/static/js/main.54d904c7.chunk.js"
+  },
+  {
+    "revision": "ca880742f67e1ffe03e9",
+    "url": "/swapi/static/js/runtime-main.22a7b150.js"
+  },
+  {
+    "revision": "ec1e59170b056cacd2083b64d2e0ebe0",
+    "url": "/swapi/static/media/300_182px-Star_Wars_Logo.ec1e5917.png"
+  },
+  {
+    "revision": "91470f01bf28fbf780902843e841ddb9",
+    "url": "/swapi/static/media/480_290px-Star_Wars_Logo.91470f01.png"
+  },
+  {
+    "revision": "bd76d377e95faa286b23f04b826ffa6a",
+    "url": "/swapi/static/media/800_483px-Star_Wars_Logo.bd76d377.png"
+  },
+  {
+    "revision": "138aae2656a3887db746aa63824e4922",
+    "url": "/swapi/static/media/Star_Wars_Logo._225x137.138aae26.png"
+  },
+  {
+    "revision": "acf2488d432c884ef57ad48b37f3c2b6",
+    "url": "/swapi/static/media/pexels-free-nature-stock-1005309.acf2488d.jpg"
+  }
+]);
